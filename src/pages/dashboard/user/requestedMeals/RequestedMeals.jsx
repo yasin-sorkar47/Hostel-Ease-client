ce2caf63bc6c010f83adcca5289ee735d711b5df
yasin-sorkar47 <mdyacinsorkar17@gmail.com>
2025-01-19 01:02:43 +0600

    compleated the reqeste meals page

--- src/pages/dashboard/user/requestedMeals/RequestedMeals.jsx
+++ src/pages/dashboard/user/requestedMeals/RequestedMeals.jsx
@@ -1,11 +1,12 @@
 import { useQuery } from "react-query";
+import Swal from "sweetalert2";
 import useAuth from "../../../../hooks/useAuth";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 
 export default function RequestedMeals() {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-  const { data: requestedMeals = [] } = useQuery({
+  const { data: requestedMeals = [], refetch } = useQuery({
     queryKey: ["requested-meals"],
     queryFn: async () => {
       const { data } = await axiosSecure(`/requested-meals/${user?.email}`);
@@ -14,7 +15,30 @@ export default function RequestedMeals() {
   });
 
   const handleRequestCancel = (id) => {
-    console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Do you want to cancel this request!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, cancel it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const { data } = await axiosSecure.patch(`/requested-meals/${id}`, {
+          status: "canceled",
+        });
+
+        if (data.modifiedCount > 0) {
+          refetch();
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your request has been canceled.",
+            icon: "success",
+          });
+        }
+      }
+    });
   };
 
   return (
@@ -43,8 +67,9 @@ export default function RequestedMeals() {
                   <td>{meal.request_status}</td>
                   <td>
                     <button
+                      disabled={meal.request_status !== "pending"}
                       onClick={() => handleRequestCancel(meal._id)}
-                      className="border px-4 py-1 border-purple-400"
+                      className="border px-4 py-1 border-purple-400 disabled:cursor-not-allowed"
                     >
                       cancel request
                     </button>
