b4b53f0ad149b5507e4a45eeb889e1ad6215d8f6
yasin-sorkar47 <mdyacinsorkar17@gmail.com>
2025-01-24 20:21:42 +0600

    codified

--- src/hooks/useAxiosPublick.jsx
+++ src/hooks/useAxiosPublick.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "https://hostelease-1773a.web.app",
+  baseURL: "https://education-services-server-a-10.vercel.app",
   withCredentials: true,
 });
 
